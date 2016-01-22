bec02551186e007cbca3a73add0c275d33fdd637
Kimyuhwan <yuhwan@nclab.kaist.ac.kr>
2016-01-22 11:41:04 +0900

    commit for dummy files

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -75,7 +75,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -125,7 +125,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     controller: 'gameCtrl'
   });
 
-
+  $ionicConfigProvider.views.swipeBackEnabled(false);
 
   //.state('home', {
   //  url: '/home',
