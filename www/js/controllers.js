42c78f6c940b2d9194a8137c039d6a29ea5499e3
Kimyuhwan <yuhwan@nclab.kaist.ac.kr>
2016-02-01 18:07:13 +0900

    Add timeline plugin
    to overall page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,54 @@ angular.module('starter.controllers', [])
     /* 진행상황 확인 페이지 */
     console.log('Start: Overall Ctrl');
 
+    // timeline
+    $scope.timeline = [{
+        date: new Date(),
+        title: "I am here",
+        author:"Ludo Anderson",
+        profilePicture:"perry.png",
+        text: "Lorem ipsum dolor sit amet",
+        type: "location"
+
+      },{
+        date: new Date(),
+        title: "For my friends",
+            author:"Sara Orwell",
+            profilePicture:"mike.png",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        type: "text"
+      },{
+        date: new Date(),
+        title: "Look at my video!",
+        author:"Miranda Smith",
+        profilePicture:"max.png",
+        text: "Lorem ipsum dolor sit amet",
+        type: "video"
+
+      },{
+        date: new Date(),
+        title: "Awesome picture",
+        author:"John Mybeweeg",
+        profilePicture:"adam.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        type: "picture"
+      },{
+        date: new Date(),
+        title: "Awesome picture",
+        author:"John Mybeweeg",
+        profilePicture:"adam.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        type: "picture"
+      },{
+        date: new Date(),
+        title: "Awesome picture",
+        author:"John Mybeweeg",
+        profilePicture:"adam.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        type: "picture"
+      }];
+
+
     $scope.code = "";
     // socket functions
     $scope.socket = $socket.getSocket();
